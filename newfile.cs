ea824184117ea5e685757943b4cf09262cbeb037
chris <borton4@gmail.com>
2023-01-20 20:36:00 -0500

    ammend

---
+++ newfile.cs
@@ -0,0 +1,19 @@
+ //This is a c# program that prints out "Hello World"
+    using System;
+    using System.IO;
+    using System.Text;
+
+    namespace IncludeHelp
+    {
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                //This is line will print out "Hello World"
+                Console.WriteLine("Hello World");
+
+                //This let's us exit the porgram when you press enter 
+                Console.ReadLine();
+            }
+        }
+    }
